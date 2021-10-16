37d618f6617a92c9be989a9b35cb084a0b7c5ea4
Luiz Ribeiro <luizribeiro@gmail.com>
2021-10-16 11:22:40 -0500

    Basic template usage

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from typing import Sequence, Tuple
 from zoneinfo import ZoneInfo
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -68,87 +68,20 @@ def get_day_of_the_week() -> int:
 @app.route("/")
 def index():
     lunch_menu, dinner_menu = get_menu()
-
-    output = """
-    <html>
-    <head>
-        <title>Menu</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-        body {
-            font-family: 'Open Sans', sans-serif;
-            font-weight: 400;
-            color: #2d3239;
-        }
-
-        b {
-            font-weight: 700;
-        }
-
-        html {
-            background-color: #eee;
-            margin: auto;
-            padding: 10px;
-        }
-
-        body {
-            border: 1px solid #ccc;
-            block-size: fit-content;
-            max-width: max-content;
-            margin: auto;
-            padding: 1em 2em 1em 2em;
-            background-color: #fff;
-        }
-
-        h1 {
-            text-align: center;
-        }
-
-        ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-        }
-
-        body > ul > ul {
-            padding-bottom: 2em;
-        }
-
-        body > ul > li {
-            text-align: center;
-            padding: 0.25em;
-        }
-
-        .today {
-        }
-
-        .future {
-            color: #999;
-        }
-        </style>
-    </head>
-    <body>
-    <h1>🍽️ Menu</h1>
-    """
     current_day = get_day_of_the_week()
-    output += "<ul>"
+    content = "<ul>"
     for i, day in enumerate(DAYS):
         if i < current_day:
             continue
         klass = "today" if i == current_day else "future"
-        output += f'<li class="{klass}"><b>{day}</b></li>'
-        output += '<ul class="{klass}">'
-        output += f'<li class="{klass}"><b>Lunch:</b> {lunch_menu[i]}</li>'
-        output += f'<li class="{klass}"><b>Dinner:</b> {dinner_menu[i]}</li>'
-        output += "</ul>"
-    output += "</ul>"
-    output += "</body>"
-    output += "</html>"
+        content += f'<li class="{klass}"><b>{day}</b></li>'
+        content += '<ul class="{klass}">'
+        content += f'<li class="{klass}"><b>Lunch:</b> {lunch_menu[i]}</li>'
+        content += f'<li class="{klass}"><b>Dinner:</b> {dinner_menu[i]}</li>'
+        content += "</ul>"
+    content += "</ul>"
 
-    return output
+    return render_template("index.html.jinja", content=content)
 
 
 @app.route("/api/today")
