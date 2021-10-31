e387356fba837fce7aedfd599c3e5d4f8fccee30
Luiz Ribeiro <luizribeiro@gmail.com>
2021-10-31 16:53:32 -0500

    Basic version of grocery list on the cli

--- cli.py
+++ cli.py
@@ -1,9 +1,11 @@
 import click
 import dateparser
+from typing import Dict
 
 import constants
 from flask import Flask
 from menu import get_menu
+from menu.recipes import get_recipe
 from utils import cache
 
 
@@ -22,3 +24,27 @@ def menu(when: str) -> None:
         print(f"  Dinner: {dinner_menu[i]}")
         if i < 6:
             print()
+
+
+@click.command()
+@click.argument("when", default="today")
+def groceries(when: str) -> None:
+    date = dateparser.parse(when)
+    lunch_menu, dinner_menu = get_menu(date)
+    all_recipes = {
+        name: get_recipe(name)
+        for name in (list(lunch_menu) + list(dinner_menu))
+    }
+    shopping_list: Dict[str, int] = {}
+    for recipe_name, recipe in all_recipes.items():
+        if len(recipe.ingredients) == 0:
+            print(
+                f"WARNING: recipe {recipe_name} does not have any ingredients"
+            )
+        for ingredient in recipe.ingredients:
+            name = ingredient.name.lower()
+            if name not in shopping_list.keys():
+                shopping_list[name] = 0
+            shopping_list[name] += 1
+    for ingredient in sorted(shopping_list.keys()):
+        print(f"{ingredient}: {shopping_list[ingredient]}")
