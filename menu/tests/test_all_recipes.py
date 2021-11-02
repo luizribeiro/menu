fa2f9c1cc4db0c5f70b9dd51c7793e470ba9de53
Luiz Ribeiro <luizribeiro@gmail.com>
2021-11-02 18:39:05 -0500

    Added test to make sure all ingredients have colors

--- menu/tests/test_all_recipes.py
+++ menu/tests/test_all_recipes.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from pyexpect import expect
 
+import constants
 from menu.recipes import get_all_recipes, get_recipe
 
 
@@ -11,3 +12,12 @@ class RecipeTest(TestCase):
         for recipe in recipes:
             cooklang_recipe = get_recipe(recipe.name)
             expect(cooklang_recipe).different(None)
+
+    def test_all_ingredients_have_colors(self) -> None:
+        recipes = get_all_recipes()
+        all_ingredients = {
+            ingredient.name.lower().strip()
+            for recipe in recipes
+            for ingredient in get_recipe(recipe.name).ingredients
+        }
+        expect(all_ingredients - set(constants.COLORS.keys())).to_be_empty()
