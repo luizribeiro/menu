d1c6d59fdb391c99fb3256b41acbb013cf93ec87
Luiz Ribeiro <luizribeiro@gmail.com>
2021-12-11 17:38:32 -0600

    Added more tests

--- menu/tests/test_planner.py
+++ menu/tests/test_planner.py
@@ -60,3 +60,33 @@ class MealPlannerTest(TestCase):
                 ("Quinoa bowls", "Roasted sweet potatoes"),
             ]
         )
+
+    @freeze_time(datetime(2021, 12, 5, tzinfo=config.get_timezone()))
+    def test_dec_11(self) -> None:
+        menu = list(zip(*get_menu(datetime.now(config.get_timezone()))))
+        expect(menu).to_equal(
+            [
+                ("Rice and beans", "Stuffed bell peppers"),
+                ("Omelet", "Tortellini soup"),
+                ("Shakshuka", "Pizza"),
+                ("Soylent", "Torta salgada"),
+                ("Bagel with egg", "Pea soup"),
+                ("Mediterranean salad", "Lentil dahl"),
+                ("Chickpea salad", "Mushroom Risotto"),
+            ]
+        )
+
+    @freeze_time(datetime(2021, 12, 12, tzinfo=config.get_timezone()))
+    def test_dec_12(self) -> None:
+        menu = list(zip(*get_menu(datetime.now(config.get_timezone()))))
+        expect(menu).to_equal(
+            [
+                ("Farro salad", "Roasted veggies + tenderloin"),
+                ("Savory pancakes", "Winter vegetable bowls"),
+                ("Ravioli", "Pita bread with baharat cauliflower"),
+                ("Mediterranean salad", "Bread + cheese + olives"),
+                ("Bagel with egg", "Pasta al Funghi"),
+                ("Soylent", "Lentils with rice"),
+                ("Gnocchi with pumpkin", "Tacos"),
+            ]
+        )
